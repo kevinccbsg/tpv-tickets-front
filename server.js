5dc21a115e3982f4e2b5c62fdb705489ff6af474
Briam Martinez <briammartinez@gmail.com>
2020-01-23 23:00:26 +0100

    fix helmet and compression require

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
+/* eslint-disable */
 const express = require('express');
+const { join } = require('path');
+const helmet = require('helmet');
+const compression = require('compression');
 
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send(JSON.stringify({ Hello: 'world' }));
+app.use(helmet());
+app.use(compression());
+
+app.use(express.static('dist'));
+app.get('/*', (req, res) => {
+  res.sendFile(join(__dirname, 'dist', 'index.html'));
 });
-app.listen(port, () => {
-  console.log('Example app listening on port !');
-});
+
+app.listen(PORT, () =>
+  console.log(`Listening PORT: ${PORT}`)
+);
