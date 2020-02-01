e5cf4a59957a9bb074e77e2640aa86b89c46a457
Briam Martinez Escobar <briammartinez@gmail.com>
2020-02-01 13:17:12 +0100

    Feature/storybook (#23)
    * Added storybook dependencies
    
    * Created .storybook folder with addons and config
    
    * Created the first storie with the BkButton component
    
    * Fix sass errors in storybook. Ass sass loader and @ alias in webpack's resolve option
    
    Co-authored-by: Kevin Julián Martínez Escobar <kevinccbsg@gmail.com>

---
+++ src/components/BkButton/BkButton.stories.js
@@ -0,0 +1,13 @@
+import { storiesOf } from '@storybook/vue';
+
+
+import BkButton from './BkButton.vue';
+
+
+storiesOf('Components/BkButton', module)
+  .add('default', () => ({
+    components: { BkButton },
+    template: `
+      <BkButton>Login</BkButton>
+        `,
+  }));
