92880fd5c1ce31fe6b749ef3b6098ae21df32403
Briam Martinez <briammartinez@gmail.com>
2020-02-02 23:14:43 +0100

    Fixed formatCurrency format

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,10 +1,1 @@
-const validateNumber = value => (value.includes(',') ? value.split(',').join('') : value);
-
-export const formatCurrency = (value) => {
-  const validValue = validateNumber(value);
-  return Number(validValue).toLocaleString('de-DE', {
-    style: 'currency',
-    currency: 'EUR',
-    minimumFractionDigits: 0,
-  });
-};
+export const formatCurrency = value => `${value} €`;
